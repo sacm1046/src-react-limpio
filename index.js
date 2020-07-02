dbb3aefcea66f601a1818664660d443c100b5717
Sebastian Contreras <sacm1046@gmail.com>
2020-07-01 21:38:03 -0400

    src vacío react

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import Layout from './layout';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
+
